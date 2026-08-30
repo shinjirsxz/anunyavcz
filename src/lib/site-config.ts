2f966e6e3de284cd6b54e69fd65342cfd9329fd1
shinjirsxz <moelhaen8895@gmail.com>
2026-08-31 03:07:24 +0700

    Update site-config.ts

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,5 +1,9 @@
 export const FAVICON_URL = "https://cdn.nekohime.site/file/7pf7r62i.jpeg";
 
+// Gambar preview (Open Graph) yang muncul saat link website di-share, contoh: di WhatsApp/Telegram/Discord.
+export const OG_IMAGE_URL =
+  "https://iad.microlink.io/nk-L3M_4kB-axU4fsqBOZfxf19TgNmX0nDvwGKWGbTaODMS7VAXhHzSP4yVUcfpcEfXtEbru09gFh5j3rXyRPg.png";
+
 export const WA_URL_SELECTION = "https://chat.whatsapp.com/J5NHH868UBZAtmSfPUeNSh";
 
 export const WA_URL_DIRECT = "https://chat.whatsapp.com/JqBKCwH0LAtJdzFvYf9kEe";
